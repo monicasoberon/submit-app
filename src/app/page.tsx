3e75261ea62eca6714a1d4f57f340feaf0b3d940
Mónica Soberón Zubía <monicasoberon2747@gmail.com>
2025-03-03 12:57:26 -0600

    3marzo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Counter(){
   return (
     <main className="flex flex-col min-h-[calc(100vh-73px)] items-center justify-center">
       <h1 className="flex-4xl font-bold text-gray-800">{counter}</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} >
         <button
           type="submit"
           className="w-full bg-blue-600 text-white rounded-md px-4 py-2 hover:bg-blue-700 transition-colors"
